fcfe3d79235d8138cf2db87e2f08543cbde0f84f
Cyrill Bolliger <mail@cyrill.me>
2024-12-03 11:18:24 +0100

    [FEAT] day 3 challenge 1
    Signed-off-by: Cyrill Bolliger <mail@cyrill.me>

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,4 +6,5 @@ def main(): Unit = {
   println(f"Day 1, Challenge 2: ${day1.getSimilarityScore()}")
   println(f"Day 2, Challenge 1: ${day2.getSafeCount()}")
   println(f"Day 2, Challenge 2: ${day2.getSafeCountWithTolerance()}")
+  println(f"Day 3, Challenge 1: ${day3.getSumOfMultiplications()}")
 }
