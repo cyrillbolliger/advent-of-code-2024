3c64a90fd7708b981fc5e90b2f4c6620ca326bff
Cyrill Bolliger <mail@cyrill.me>
2024-12-04 16:58:24 +0100

    [REFACTOR] variable row buffer size

--- src/main/scala/day4/RowBuffer.scala
+++ src/main/scala/day4/RowBuffer.scala
@@ -1,14 +1,16 @@
 package me.cyrill.aoc2024.day4
 
-class RowBuffer(r0: String, r1: String, r2: String, r3: String):
-  private val rows = Array(r0, r1, r2, r3)
+class RowBuffer(rows: String*):
+  private val data = rows.toArray
+  private val dimension = rows.length
+
   private var start = 0
 
   def apply(i: Int): String =
     require(i >= 0)
-    require(i <= 3)
-    rows((i + start) % 4)
+    require(i < dimension)
+    data((i + start) % dimension)
 
   def push(r: String): Unit =
-    rows(start) = r
-    start = (start + 1) % 4
+    data(start) = r
+    start = (start + 1) % dimension
