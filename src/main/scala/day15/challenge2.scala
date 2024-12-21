b728f5bfdbea7f2c18eb880582e416857aa209a4
Cyrill Bolliger <mail@cyrill.me>
2024-12-21 20:14:15 +0100

    [FEAT] day 15 challenge 2

---
+++ src/main/scala/day15/challenge2.scala
@@ -0,0 +1,51 @@
+package me.cyrill.aoc2024.day15.challenge2
+
+import me.cyrill.aoc2024.util.pos.*
+import me.cyrill.aoc2024.util.matrix.*
+import me.cyrill.aoc2024.day15.{parseMoves, moveRecursive, Warehouse}
+
+def parseWarehouse(input: Array[String]): Warehouse =
+  val data = input
+    .takeWhile(_ != "")
+    .map(_.toArray.flatMap(_ match
+      case '#' => Array('#', '#')
+      case 'O' => Array('[', ']')
+      case '.' => Array('.', '.')
+      case '@' => Array('@', '.')
+      case c   => throw IllegalArgumentException(f"Unknown map char: $c")
+    ))
+
+  new Warehouse(data)
+
+def moveRecVertical(
+    state: Warehouse,
+    pos: Pos,
+    char: Char,
+    nextPos: Pos => Pos
+): Option[Warehouse] =
+  val next = nextPos(pos)
+  state(next) match
+    case '#' => None
+    case '.' =>
+      Some(state.updated(next, char).updated(pos, '.'))
+    case '[' =>
+      moveRecVertical(state, next.east, ']', nextPos)
+        .flatMap(moveRecVertical(_, next, '[', nextPos))
+        .flatMap(moveRecVertical(_, pos, char, nextPos))
+    case ']' =>
+      moveRecVertical(state, next.west, '[', nextPos)
+        .flatMap(moveRecVertical(_, next, ']', nextPos))
+        .flatMap(moveRecVertical(_, pos, char, nextPos))
+
+def move(state: Warehouse, mv: Char): Option[Warehouse] =
+  mv match
+    case '^' => moveRecVertical(state, state.botPos, '@', _.north)
+    case '>' => moveRecursive(state, state.botPos, '@', _.east)
+    case 'v' => moveRecVertical(state, state.botPos, '@', _.south)
+    case '<' => moveRecursive(state, state.botPos, '@', _.west)
+    case _   => throw IllegalArgumentException(f"Unknown move: $mv")
+
+def solve(input: Array[String]): Int =
+  val state = parseWarehouse(input)
+  val moves = parseMoves(input)
+  moves.foldLeft(state)((s, m) => move(s, m).getOrElse(s)).boxCoords('[').sum
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -20,7 +20,7 @@ def main(): Unit =
     List(() => day12.solve1, () => day12.solve2),
     List(() => day13.solve1, () => day13.solve2),
     List(() => day14.solve1, () => day14.solve2),
-    List(() => day15.solve1)
+    List(() => day15.solve1, () => day15.solve2)
   )
 
   days.par // fork
